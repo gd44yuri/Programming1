e557c9021084579d58991aa04b77be22ac938659
unknown <gd44yuri@GD107.vfs.local>
2016-06-19 17:23:35 -0700

    Title screen.

---
+++ Programming Assignment 3/TitleScreen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Assignment_3
+{
+    class TitleScreen
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\\\\\\\                             █■▄");
+            Console.WriteLine("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■████■");
+            Console.WriteLine("////                             █■▀");
+            Console.WriteLine("▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
+            Console.WriteLine("█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█");
+            Console.WriteLine("█▒▒▒▄▄▄▄▒▄▄▄▒▒▒▄▄▄▒▄▒▒▒▄▒▄▄▄▄▒▄▄▄▒▒▒▒█");
+            Console.WriteLine("█▒▒▒█▄▄█▒█▄▄█▒█▒▒▒▒█▄▄▄█▒█▄▄▒▒█▄▄█▒▒▒█");
+            Console.WriteLine("█▒▒▒█▒▒█▒█▒▀▄▒▀▄▄▄▒█▒▒▒█▒█▄▄▄▒█ ▀▄▒▒▒█");
+            Console.WriteLine("█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█");
+            Console.WriteLine("█▒▒▄▄▄▒▄▒▒▒▄▒▒▄▄▄▒▒▒▄▄▄▒▒▄▄▄▄▄▒▄▒▒▒▄▒█");
+            Console.WriteLine("█▒█▄▄▒▒█▄▄▄█▒█▒▒▒█▒█▒▒▒█▒▒▒█▒▒▒█▄▄▄█▒█");
+            Console.WriteLine("█▒▄▄▄█▒█▒▒▒█▒▀▄▄▄▀▒▀▄▄▄▀▒▒▒█▒▒▒▒▒█▒▒▒█");
+            Console.WriteLine("█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█");
+            Console.WriteLine("█▒▒▒▒▒▄▄▄▒▄▒▒▒▄▒▒▄▄▄▒▒▒▄▄▄▒▒▄▄▄▄▄▒▒▒▒█");
+            Console.WriteLine("█▒▒▒▒█▄▄▒▒█▄▄▄█▒█▒▒▒█▒█▒▒▒█▒▒▒█▒▒▒▒▒▒█");
+            Console.WriteLine("█▒▒▒▒▄▄▄█▒█▒▒▒█▒▀▄▄▄▀▒▀▄▄▄▀▒▒▒█▒▒▒▒▒▒█");
+            Console.WriteLine("█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█");
+            Console.WriteLine("▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀");
+            Console.WriteLine("\\\\\\\\                             █■▄");
+            Console.WriteLine("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■████■");
+            Console.WriteLine("////                             █■▀");
+
+            Console.ReadLine();
+        }
+    }
+}
